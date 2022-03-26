dcbd51714137f57cb86cb8340944dfc0987b1114
99x-dgCandidate-Ratheshan-1642831919908-at-675482622082 <ratheshan.20200170@iit.ac.lk>
2022-03-26 15:56:37 +0530

    starting Home page build

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,6 +1,8 @@
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
-import { FlatList, Image, TouchableOpacity } from "react-native-web";
+import tw from "tailwind-react-native-classnames";
+import { Icon } from "react-native-elements";
+import { useNavigation } from "@react-navigation/native";
 
 const data = [
   {
@@ -11,23 +13,36 @@ const data = [
   },
   {
     id: "456",
-    title: "Order fodd",
+    title: "Order food",
     image: "https://links.papareact.com/28w",
     screen: "EatsScreen", //Change in future
   },
 ];
 
 const NavOptions = () => {
+  const navigation = useNavigation();
+
   return (
     <FlatList
       data={data}
+      horizontal
       keyExtracter={(item) => item.id}
       renderItem={({ item }) => (
-        <TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => navigation.navigate(item.screen)}
+          style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2`}
+        >
           <View>
             <Image
               style={{ width: 120, height: 120, resizeMode: "contain" }}
               source={{ uri: item.image }}
+            />
+            <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
+            <Icon
+              style={tw`p-2 bg-black rounded-full w-10 mt-4`}
+              type="antdesign"
+              name="arrowright"
+              color="white"
             />
           </View>
         </TouchableOpacity>
@@ -37,5 +52,3 @@ const NavOptions = () => {
 };
 
 export default NavOptions;
-
-const styles = StyleSheet.create({});
